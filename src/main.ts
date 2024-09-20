ed6670770d58dfa5a8059ccdd0b02163a3bf691a
Leonardo Salema Dev <leo_viciogta@hotmail.com>
2024-09-19 21:52:46 -0300

    feat: page cassino criada

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,12 @@ import App from "./App.vue";
 import { createWebHistory, createRouter } from "vue-router";
 
 import Home from "./pages/Home.vue";
+import Cassino from "./pages/Cassino.vue";
 
-const routes = [{ path: "/", component: Home }];
+const routes = [
+  { path: "/", component: Home },
+  { path: "/cassino", component: Cassino },
+];
 
 const router = createRouter({
   history: createWebHistory(),
---
+++ src/pages/Cassino.vue
@@ -0,0 +1,60 @@
+<script setup>
+import { cassinoList } from "../utils/cassinoList";
+</script>
+
+<template>
+  <div class="game_list_container">
+    <h2>Todos os jogos</h2>
+
+    <ul class="game_list">
+      <li class="game" v-for="game in cassinoList" :key="game">
+        <RouterLink :to="`/cassino/${game.link}`">
+          <img :src="game.image" :alt="`icon-${game.name}`" />
+          {{ game.name }}
+        </RouterLink>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style scoped>
+h2 {
+  color: #fff;
+  text-align: center;
+}
+
+.game_list_container {
+  width: 100%;
+}
+
+.game_list {
+  display: flex;
+  align-items: center;
+  margin-top: 2em;
+}
+
+.game {
+  padding: 12px 24px;
+  gap: 12px;
+  background-color: #0f1923;
+  transition: .3s;
+
+
+  &:hover {
+    box-shadow: 0px 0px 16px #ffffff0c;
+    background-color: #ffffff0c;
+    transition: .3s;
+  }
+}
+
+.game a {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.game img {
+  max-width: 80px;
+  margin-bottom: 12px;
+}
+</style>
