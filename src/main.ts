d81fa1428f3e8a8bde1dc7fe5216cc7dcb2a6190
Leonardo Salema Dev <leo_viciogta@hotmail.com>
2024-09-19 21:49:32 -0300

    chore: configurando rotas para o projeto

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,15 @@ import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
 
-createApp(App).mount("#app");
+import { createWebHistory, createRouter } from "vue-router";
+
+import Home from "./pages/Home.vue";
+
+const routes = [{ path: "/", component: Home }];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+createApp(App).use(router).mount("#app");
