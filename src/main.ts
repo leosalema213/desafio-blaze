5254b02b2d8450e224dafb59d04e2d5c951c2ef4
Leonardo Salema Dev <leo_viciogta@hotmail.com>
2024-09-19 21:54:17 -0300

    feat: page de esportes criada

--- src/main.ts
+++ src/main.ts
@@ -6,14 +6,18 @@ import { createWebHistory, createRouter } from "vue-router";
 
 import Home from "./pages/Home.vue";
 import Cassino from "./pages/Cassino.vue";
+import Double from "./pages/Double.vue";
 import Register from "./pages/Register.vue";
 import Login from "./pages/Login.vue";
+import Esportes from "./pages/Esportes.vue";
 
 const routes = [
   { path: "/", component: Home },
   { path: "/cassino", component: Cassino },
   { path: "/register", component: Register },
+  { path: "/cassino/double", component: Double },
   { path: "/login", component: Login },
+  { path: "/esportes", component: Esportes },
 ];
 
 const router = createRouter({
---
+++ src/pages/Esportes.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="esportes_container">
+    <h2>Em breve...</h2>
+
+    <img src="../assets/contruction-animation.svg" alt="">
+  </div>
+</template>
+
+<style scoped>
+h2 {
+  font-size: 2em;
+  color: #fff;
+}
+
+img {
+  width: 80%;
+}
+
+.esportes_container {
+  text-align: center;
+  width: 100%;
+}
+</style>
